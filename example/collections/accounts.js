bf5089efad0cec24c7d563302a22ac9a092a4c1f
Court Ewing <court@epixa.com>
2015-07-24 11:22:26 -0400

    Syntax error in example collection

--- example/collections/accounts.js
+++ example/collections/accounts.js
@@ -7,7 +7,7 @@ export class Accounts extends Collection {
     this.model('account');
     this.plural('accounts'); // optional, default behavior of model() will set this
 
-    this.filter('perPage', () => , (n=50) => `per_page=${n}`);
+    this.filter('perPage', (n=50) => `per_page=${n}`);
     this.filter('page', (p=1) => `page=${p}`);
   }
 }
