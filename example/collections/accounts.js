1074588aaa23cf217c59ae4153a99d9014e20bd1
Court Ewing <court@epixa.com>
2015-07-24 16:04:28 -0400

    More concise setting of custom view

--- example/collections/accounts.js
+++ example/collections/accounts.js
@@ -8,7 +8,7 @@ export class Accounts extends Collection {
     this.model('account');
     this.plural('accounts'); // optional, default behavior of model() will set this
 
-    this.defaulView(AccountsView);
+    this.view(AccountsView); // optional, only necessary if you want a custom View
 
     this.filter('perPage', (n=50) => `per_page=${n}`);
     this.filter('page', (p=1) => `page=${p}`);
