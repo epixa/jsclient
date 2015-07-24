d5401b2ea7ed03631f0584ae38c43f06198a35d1
Court Ewing <court@epixa.com>
2015-07-24 11:32:10 -0400

    Custom views

--- example/collections/accounts.js
+++ example/collections/accounts.js
@@ -1,11 +1,14 @@
 'use strict';
 
 import { Collection } from '../collection';
+import { AccountsView } from '../views/accounts';
 
 export class Accounts extends Collection {
   init() {
     this.model('account');
     this.plural('accounts'); // optional, default behavior of model() will set this
+
+    this.defaulView(AccountsView);
 
     this.filter('perPage', (n=50) => `per_page=${n}`);
     this.filter('page', (p=1) => `page=${p}`);
---
+++ example/views/accounts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import { View } from '../../lib/view';
+
+export class AccountsView extends View {
+  init() {
+    // for anything that needs to happen at instantiation
+  }
+
+  someCustomFunc() {
+    
+  }
+}
