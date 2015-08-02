e9c989e257cc19e9dee6801ad03e6f9b399f5585
Josh Lasdin <joshlasdindesign@gmail.com>
2015-08-02 02:30:08 -0400

    Added karma & mocha/chai/sinon for testing.

---
+++ test/index.js
@@ -0,0 +1,8 @@
+// Test entry file that pulls in all the spec files. Keeps tests to a single
+// bundle, and allows us to use webpack in the tests.
+//
+// http://nicolasgallagher.com/how-to-test-react-components-karma-webpack/
+
+var context = require.context('.', true, /.+\_spec\.js?$/);
+context.keys().forEach(context);
+module.exports = context;
