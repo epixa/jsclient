eadaf41b5573caed0a1d6eee5bff4d1aa49a9172
Court Ewing <court@epixa.com>
2015-08-02 12:36:08 -0400

    Base client Request
    Requests are objects with a $send method that consumers will use to
    configure and perform specific xhr requests.

--- test/assert.spec.js
+++ test/assert.spec.js
@@ -14,4 +14,39 @@ describe('assert', () => {
       expect(() => assert.isFunction(arg)).not.to.throw();
     })
   });
+
+  describe('#notEmpty', () => {
+    it('throws if value is null', () => {
+      expect(() => assert.notEmpty(null)).to.throw();
+    });
+    it('throws if value is undefined', () => {
+      expect(() => assert.notEmpty()).to.throw();
+    });
+    it('throws if value is empty string', () => {
+      expect(() => assert.notEmpty('')).to.throw();
+    });
+    it('throws if value is 0', () => {
+      expect(() => assert.notEmpty(0)).to.throw();
+    });
+    it('throws if value is an empty array', () => {
+      expect(() => assert.notEmpty([])).to.throw();
+    });
+    it('noops if arg is not empty', () => {
+      expect(() => assert.notEmpty('foo')).not.to.throw();
+      expect(() => assert.notEmpty(1)).not.to.throw();
+      expect(() => assert.notEmpty([1])).not.to.throw();
+    });
+  });
+
+  describe('#isString', () => {
+    it('throws if arg is not a string', () => {
+      expect(() => assert.isString()).to.throw();
+      expect(() => assert.isString(null)).to.throw();
+      expect(() => assert.isString(123)).to.throw();
+      expect(() => assert.isString({})).to.throw();
+    });
+    it('noops if arg is a string', () => {
+      expect(() => assert.isString('')).not.to.throw();
+    });
+  });
 });
