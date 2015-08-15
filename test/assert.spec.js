99ba40701bdd52413853bc50296fa5e5abc20e3e
Court Ewing <court@epixa.com>
2015-08-15 16:12:39 -0400

    Test assert.isDefined

--- test/assert.spec.js
+++ test/assert.spec.js
@@ -3,15 +3,36 @@
 import * as assert from '../lib/assert';
 
 describe('assert', () => {
-  describe('#isFunction', () => {
-    it('throws error if arg is not a function', () => {
-      expect(() => assert.isFunction()).to.throw();
-      expect(() => assert.isFunction('foo')).to.throw();
-      expect(() => assert.isFunction({})).to.throw();
+  describe('#isFunction()', () => {
+    context('when given a non-function', () => {
+      it('throws', () => {
+        expect(() => assert.isFunction()).to.throw();
+        expect(() => assert.isFunction('foo')).to.throw();
+        expect(() => assert.isFunction({})).to.throw();
+      });
     })
-    it('noops if arg is a function', () => {
-      const arg = () => {};
-      expect(() => assert.isFunction(arg)).not.to.throw();
+
+    context('when given a function', () => {
+      it('does not throw', () => {
+        const arg = () => {};
+        expect(() => assert.isFunction(arg)).not.to.throw();
+      });
+    })
+  });
+
+  describe('#isDefined()', () => {
+    context('when given undefined', () => {
+      it('throws', () => {
+        expect(() => assert.isDefined()).to.throw();
+      });
+    })
+
+    context('when given a defined value', () => {
+      it('does not throw', () => {
+        expect(() => assert.isDefined(0)).not.to.throw();
+        expect(() => assert.isDefined(null)).not.to.throw();
+        expect(() => assert.isDefined('')).not.to.throw();
+      });
     })
   });
 
